79f2a225bfe2b48614081ddf47bfb78bb2aa5f42
mdybal <mdybal@live.com>
2015-01-24 12:09:20 +0100

    initial commit

---
+++ HelloWorld/HelloWorld.Shared/js/paintCanvas/paintingReader.js
@@ -0,0 +1,8 @@
+﻿(function () {
+
+    this.img = document.createElement("img");
+    this.img.src = "images/prussian_homage.jpg";
+
+    var paintingDiv = document.getElementById("painting");
+    paintingDiv.appendChild(this.img);
+})();
